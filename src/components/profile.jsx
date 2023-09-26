53d81d84ce2d8e0462cfb0022d018eb92dfda562
Hayden <hgwheele@geneva.edu>
2023-09-26 12:59:51 -0400

    base

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -8,9 +8,19 @@ const Profile = () => {
                 <Navbar />
             </header>
             <main>
-                <h1>Profile</h1>
+                <center>
+                <h1>Profile   <button type="button">Edit Profile</button></h1>
+                <p><b>First Name:</b> ((temp))</p>
+                <p><b>Last Name:</b> ((temp))</p>
+                <p><b>Username:</b> ((temp))</p>
+                <p><b>Email:</b> ((temp))</p>
+                <p><b>Password:</b> ((temp)) <button type="button">Reveal</button></p>
+                <p><b>temp:</b> ((temp))</p>
+                <p><b>temp:</b> ((temp))</p>
+                </center>
             </main>
             <footer>
+
                 <Footer />
             </footer>
         </div>
