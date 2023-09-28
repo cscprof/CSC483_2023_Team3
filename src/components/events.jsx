05a5779575771b5831f303228052074e2f1eff9c
Ztmartin315 <ztmartin315@gmail.com>
2023-09-28 10:52:37 -0400

    Events and Upload Updated

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Table from "./table";
+import "../assets/events.css";
 const Events = () => {
     return (
         <div className="Events">
             <header>
                 <Navbar />
+                <div className="jumbo">
+                    <h1>EXPERIENCE Geneva</h1>
+                </div>
             </header>
             <main>
-                <h1>events</h1>
+                <div className="subtitle">
+                    <h2>Events</h2>
+                </div>
+                <section className="tableCont">
+                    <Table />
+                </section>
             </main>
             <footer>
                 <Footer />
