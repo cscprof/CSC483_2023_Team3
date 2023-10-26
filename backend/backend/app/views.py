6aa1695b77459cce91bbcb4e73c5734ddeee27ed
Hayden <hgwheele@geneva.edu>
2023-10-26 11:22:43 -0400

    initial login attempt, model, axios, register

--- backend/backend/app/views.py
+++ backend/backend/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['POST'])
+def user_register(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if user_is_valid: 
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return Response("Registration successful", status=status.HTTP_201_CREATED)
+    else:
+        return Response("Registration failed", status=status.HTTP_400_BAD_REQUEST)
+
+
