4829a5e19903e7658d0c9d26109fac990f7ca008
PARKERJI <Pnguice@geneva.edu>
2023-10-31 12:57:48 -0400

    map buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,20 @@ import Home from "./components/home";
 import Help from "./components/help";
 import Events from "./components/events";
 import Profile from "./components/profile";
-import OldMain from "./components/oldMain";
+import OldMain from './components/oldMain.jsx';
 import Trivia from "./components/trivia";
 import Upload from "./components/upload";
 import Reviews from "./components/reviews";
+import Alex from "./components/Buildings/Alex";
+import Dorms from "./components/Buildings/Dorms";
+import Library from './components/Buildings/Library';
+import Music from "./components/Buildings/Music";
+import Northwood from "./components/Buildings/Northwood";
+import Other from "./components/Buildings/Other";
+import Rapp from "./components/Buildings/Rapp";
+import SandE from "./components/Buildings/SandE";
+import Sports from "./components/Buildings/Sports";
+import Studentcenter from "./components/Buildings/Studentcenter";
 
 function App() {
   return (
@@ -32,6 +42,26 @@ function App() {
         <Route path='/upload' element={<Upload />} />
 
         <Route path='/reviews' element={<Reviews />} />
+
+        <Route path='/Alex' element={<Alex />} />
+
+        <Route path='/Dorms' element={<Dorms />} />
+
+        <Route path='/Library' element={<Library />} />
+
+        <Route path='/Music' element={<Music />} />
+
+        <Route path='/Northwood' element={<Northwood />} />
+
+        <Route path='/Other' element={<Other />} />
+
+        <Route path='/Rapp' element={<Rapp />} />
+
+        <Route path='/SandE' element={<SandE />} />
+
+        <Route path='/Sports' element={<Sports />} />
+
+        <Route path='/Studentcenter' element={<Studentcenter />} />
 
       </Routes>
     </Router>
--- frontend/src/components/oldMain.jsx
+++ frontend/src/components/oldMain.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import  "../assets/oldmain.css";
+
 const OldMain = () => {
   return (
     <div className="OldMain">
@@ -17,10 +18,10 @@ const OldMain = () => {
                 </div>
                 <div class="secondpart">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis risusLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis risusLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis ri Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis risLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis ri Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis risusLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis risus</p>
-                </div>
                 <div class="button">
                   <button> Upload your own photo!
                   </button>
+                </div>
                 </div>
             </main><footer>
         <Footer />
