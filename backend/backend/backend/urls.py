eb7bb4d97d151b4476c65838fcca8e9f8d494fb7
Ztmartin315 <ztmartin315@gmail.com>
2023-11-16 12:06:04 -0500

    Review Display Basic

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api/login/', views.custom_login, name='api-login'), 
     path('api/register/', views.register_user, name='register_user'),
     path('api/makeEvent/', views.make_event, name='make_event'),
-     path('api/events/', views.get_events, name='get_events'),
-      path('api/make_review/', views.submit_review, name='make_review'),
+    path('api/events/', views.get_events, name='get_events'),
+    path('api/make_review/', views.submit_review, name='make_review'),
+    path('api/get_reviews/', views.get_reviews, name='get_reviews'),
 ]
