93ed0ba27402c019c91671b7c37131f2ff93dd45
Ztmartin315 <ztmartin315@gmail.com>
2023-11-16 11:31:43 -0500

    Review Creation and Help Page Touch Ups

--- backend/backend/app/models.py
+++ backend/backend/app/models.py
@@ -12,3 +12,21 @@ class Events(models.Model):
       location = models.CharField(max_length=64)
       def __str__(self):
         return self.title
+      
+class Reviews(models.Model):
+    title = models.CharField(max_length=24)
+    date = models.CharField(max_length=24)
+    q1 = models.IntegerField()
+    q2 = models.IntegerField()
+    q3 = models.IntegerField()
+    q4 = models.IntegerField()
+    q5 = models.IntegerField()
+    q6 = models.IntegerField()
+    q7 = models.IntegerField()
+    q8 = models.IntegerField()
+    q9 = models.IntegerField()
+    q10 = models.IntegerField()
+    comments = models.CharField(max_length=500)
+    def __str__(self):
+        return self.title
+      
--- frontend/src/components/reviews.jsx
+++ frontend/src/components/reviews.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import { NavLink } from 'react-router-dom';
 const Reviews = () => {
     return (
         <div className="Reviews">
             <header>
                 <Navbar />
+                <div className="jumbo">
+                    <h1>EXPERIENCE Geneva</h1>
+                </div>
             </header>
             <main>
-                <h1>Reviews</h1>
+                <div className="subtitle">
+                    <h2>Reviews</h2>
+                </div>
+                <NavLink to="/make">
+                    <button className="make-button">Leave a Review!</button>
+                </NavLink>
             </main>
             <footer>
                 <Footer />
