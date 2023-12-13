0b498eb0b2b303ea2a9f2a73b35f115517ca9409
Ztmartin315 <ztmartin315@gmail.com>
2023-12-13 16:46:58 -0500

    trivia function cleanup

--- frontend/src/components/Buildings/Studentcenter.jsx
+++ frontend/src/components/Buildings/Studentcenter.jsx
@@ -23,18 +23,19 @@ const StudentCenter = () => {
             <div className="col">
               <h1 className="sectTitle">Notable Features</h1>
               <ul className="noteList">
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-                <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+                <li><b>Skye Lounge: </b>A useful study area on campus for all students. Balconies off Skye Lounge and the Brig feature a beautiful overlook the Beaver River and the valley.</li>
+                <li><b>The Brigadoon, AKA the “Brig”:</b> An alternative dining facility to the cafeteria, with a variety of convenient food options. The Brig also contains the Riverview Café, which proudly brews Starbucks Coffee.</li>
+                <li><b>The Campus Bookstore.</b></li>
+                <li><b>The Center for Calling and Career:</b> This office works with all students from their first year through graduation, helping them to discover and develop their calling and vocation. Services offered here include career coaching, internship and job search assistance, help with resume writing, business etiquette and interview skills, and help with grad school admission.</li>
+                <li><b>The Student Success Center:</b> Academic support, tutoring, accommodations for students with disabilities, and other workshops and seminars are available through this department.</li>
+                <li><b>The Fitness Center:</b> Accessible to students, faculty, and staff with a college I.D. card, includes a variety of cardio machines, a spacious aerobic area for flexibility training, and well-equipped weight rooms with options ranging from free weights to state-of-the-art resistance equipment.</li>
+                <li><b>Mail and Print Services.</b></li>
+                <li><b>Other:</b> Residence Life, the Center for Student Engagement, Campus Ministry, Dean of Students, Off-Campus Study, and International Student Services offices.</li>
               </ul>
             </div>
 
             <div className="col" id="imgPlace">
+              <h1 className="sectTitle">    <br /> </h1>
               <img src="https://www.geneva.edu/campusmap/_assets/images/bld-studentcenter-4.jpg" alt="placeholder"></img>
             </div>
           </div>
@@ -42,16 +43,18 @@ const StudentCenter = () => {
         <div className="notable">
           <h1 className="sectTitle">Fun Facts!</h1>
 
-          <ul id="factList">
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-            <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
-          </ul>
+          <div className="fun">
+            <ul id="factList">
+              <li>Fact 1: Skye Lounge is Named After </li>
+              <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+              <li>Fact 3: There is a Rock Climbing Wall Located In the Basement</li>
+              <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+              <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+              <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+              <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+              <li>Fact 1: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</li>
+            </ul>
+          </div>
         </div>
         <div className="gallery">
           <h1 className="sectTitle">Gallery</h1>
--- frontend/src/components/trivia.jsx
+++ frontend/src/components/trivia.jsx
@@ -1,13 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
-import '../assets/trivia.css';
+import { Modal, Button } from "react-bootstrap";
+import "../assets/trivia.css";
 import Logo from "../assets/images/trivia_image.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Trivia = () => {
+    const questions = [
+        {
+            question: "In what year was Geneva College founded?",
+            options: ["1848", "1870", "1902", "1925"],
+            correctAnswer: "1848",
+        },
+        {
+            question: "Which department offers the Computer Science major at Geneva College?",
+            options: ["Mathematics", "Business", "Computer Science", "Psychology"],
+            correctAnswer: "Computer Science",
+        },
+        {
+            question: "What is the official mascot of Geneva College?",
+            options: ["Eagle", "Lion", "Bear", "Golden Tornado"],
+            correctAnswer: "Golden Tornado",
+        },
+        {
+            question: "Which building serves as the iconic centerpiece on Geneva College's campus?",
+            options: ["Old Main", "Library", "Science Building", "Athletic Center"],
+            correctAnswer: "Old Main",
+        },
+        {
+            question: "In which state is Geneva College located?",
+            options: ["Ohio", "New York", "Pennsylvania", "Indiana"],
+            correctAnswer: "Pennsylvania",
+        },
+    ];
+
+    const [currentQuestion, setCurrentQuestion] = useState(null);
+    const [userAnswers, setUserAnswers] = useState(Array(questions.length).fill(null));
+    const [score, setScore] = useState(null);
+    const [showResultsModal, setShowResultsModal] = useState(false);
+
+    const handleAnswer = (answer) => {
+        const newAnswers = [...userAnswers];
+        newAnswers[currentQuestion] = answer;
+        setUserAnswers(newAnswers);
+    };
+
+    const handleNextQuestion = () => {
+        if (currentQuestion < questions.length - 1) {
+            setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+        }
+    };
+
+    const handlePrevQuestion = () => {
+        if (currentQuestion > 0) {
+            setCurrentQuestion((prevQuestion) => prevQuestion - 1);
+        }
+    };
+
+    const handleStart = () => {
+        setCurrentQuestion(0);
+    };
+
+    const handleSubmit = () => {
+        const newScore = userAnswers.reduce((acc, userAnswer, index) => {
+            if (userAnswer === questions[index].correctAnswer) {
+                return acc + 1;
+            }
+            return acc;
+        }, 0);
+        setScore(newScore);
+        setShowResultsModal(true);
+    };
+
+    const handleTryAgain = () => {
+        setCurrentQuestion(null);
+        setUserAnswers(Array(questions.length).fill(null));
+        setScore(null);
+        setShowResultsModal(false);
+    };
+
+    const handleCloseModal = () => {
+        setShowResultsModal(false);
+    };
+
     return (
         <div className="Trivia">
             <header>
-            <Navbar />
+                <Navbar />
                 <div className="jumbo">
                     <h1>EXPERIENCE Geneva</h1>
                 </div>
@@ -15,48 +95,107 @@ const Trivia = () => {
                     <h2>Trivia</h2>
                 </div>
             </header>
-            
+
             <main>
-                <h1 className="title">Weekly Geneva Trivia</h1>
-                <div className="imgMove">
-                <img src={Logo} className="triviaImage"></img>
-                </div>
-                    <div  className="startButton">
-                    <button>Start</button>
+                <div className="woah">
+                    <h1 className="title">Test Your Geneva Knowledge!</h1>
+                    <div className="imgMove">
+                        <img src={Logo} className="triviaImage" alt="Trivia Logo" />
                     </div>
-                    <button>View Previous Weeks Answers</button>
+                    <div className="quizArea">
+                        {score === null && currentQuestion === null ? (
+                            <div>
+                                <button className="btn btn-warning btn-outline-dark btn-lg" id="btnOof" onClick={handleStart}>
+                                    Start
+                                </button>
+                            </div>
+                        ) : (
+                            <div>
+                                {currentQuestion !== null && (
+                                    <div className="questions">
+                                        <h4>{`Question #${currentQuestion + 1}: ${questions[currentQuestion].question}`}</h4>
+                                        {questions[currentQuestion].options.map((option, index) => (
+                                            <div key={index} className="custom-control custom-radio larger-radio">
+                                                <input
+                                                    type="radio"
+                                                    id={`radio${index}`}
+                                                    name={`question${currentQuestion}`}
+                                                    value={option}
+                                                    checked={userAnswers[currentQuestion] === option}
+                                                    onChange={() => handleAnswer(option)}
+                                                    className="custom-control-input"
+                                                />
+                                                <label htmlFor={`radio${index}`} className="custom-control-label larger-radio">
+                                                    {option}
+                                                </label>
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
 
+                                <div>
+                                    {currentQuestion !== null && (
+                                        <>
+                                            <button
+                                                className="btn btn-danger"
+                                                onClick={handlePrevQuestion}
+                                                disabled={currentQuestion === 0}
+                                            >
+                                                Previous Question
+                                            </button>
+                                            <button
+                                                className="btn btn-success"
+                                                onClick={handleNextQuestion}
+                                                disabled={currentQuestion === questions.length - 1}
+                                            >
+                                                Next Question
+                                            </button>
+                                        </>
+                                    )}
 
-                <div className="questions">
-                    <h4>Question #1:</h4>
-                    <div><button>A</button><span>answer</span></div>
-                    <div><button>B</button><span>answer</span></div>
-                    <div><button>C</button><span>answer</span></div>
-                    <div><button>D</button><span>answer</span></div>
-
-                    <h4>Question #2:</h4>
-                    <div><input type="radio" id="2" name="question2" value="answer"></input>
-                    <label for="html">HTML</label></div>
-                    <div><input type="radio" id="2" name="question2" value="answer"></input>
-                    <label for="html">HTML</label></div>
-                    <div><input type="radio" id="2" name="question2" value="answer"></input>
-                    <label for="html">HTML</label></div>
-                    <div><input type="radio" id="2" name="question2" value="answer"></input>
-                    <label for="html">HTML</label></div>
-                    
+                                    {currentQuestion === questions.length - 1 && (
+                                        <button className="btn btn-success" onClick={handleSubmit}>
+                                            Submit
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                        {score !== null && (
+                            <div>
+                                <p className="score">Your Score: {score}/{questions.length}</p>
+                                <button className="btn btn-dark" onClick={() => setShowResultsModal(true)}>
+                                    View Results
+                                </button>
+                                <button className="btn btn-warning btn-outline-dark" onClick={handleTryAgain}>
+                                    Try Again
+                                </button>
+                            </div>
+                        )}
+                    </div>
                 </div>
-                <div>
-                    <button>Complete</button>
-                </div>
-
-
-
-                <div className="footer">
-                       <p>Current Score: ((01))<span>All Time High Score: ((10))</span></p>
-                </div>
-                            
-              
             </main>
+            <Modal show={showResultsModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Results</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {questions.map((question, index) => (
+                        <div key={index}>
+                            <p>{`Question #${index + 1}: ${question.question}`}</p>
+                            <p>{`Your Answer: ${userAnswers[index]}`}</p>
+                            <p>{`Correct Answer: ${question.correctAnswer}`}</p>
+                            <hr />
+                        </div>
+                    ))}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="warning" onClick={handleTryAgain}>
+                        Try Again
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <footer>
                 <Footer />
             </footer>
