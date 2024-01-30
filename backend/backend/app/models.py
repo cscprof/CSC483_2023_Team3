157965bbbe482d8b65f07ffa2ab1e594197efd9d
Zack Martin <ztmartin315@gmail.com>
2024-01-30 12:12:06 -0500

    fixins

--- backend/backend/app/models.py
+++ backend/backend/app/models.py
@@ -6,27 +6,24 @@ class User(models.Model):
     password = models.CharField(max_length=24)
 
 class Events(models.Model):
-      title = models.CharField(max_length=24)
-      date = models.CharField(max_length=24)
-      description = models.CharField(max_length=250)
-      location = models.CharField(max_length=64)
-      def __str__(self):
+    title = models.CharField(max_length=24)
+    date = models.CharField(max_length=24)
+    description = models.CharField(max_length=250)
+    location = models.CharField(max_length=64)
+
+    def __str__(self):
         return self.title
-      
+
 class Reviews(models.Model):
     title = models.CharField(max_length=24)
     date = models.CharField(max_length=24)
-    q1 = models.IntegerField()
+    q1 = models.CharField(max_length=24) 
     q2 = models.IntegerField()
     q3 = models.IntegerField()
     q4 = models.IntegerField()
     q5 = models.IntegerField()
     q6 = models.IntegerField()
-    q7 = models.IntegerField()
-    q8 = models.IntegerField()
-    q9 = models.IntegerField()
-    q10 = models.IntegerField()
     comments = models.CharField(max_length=500)
+
     def __str__(self):
         return self.title
-      
