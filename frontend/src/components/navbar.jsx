11ee5111023e3f79fc9d4a35259e2df8a4e91a02
MrHaydenn <haydenwheeler21@outlook.com>
2024-02-06 09:23:41 -0500

    Mobile CSS, Navbar CSS

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -69,12 +69,10 @@ const Navbar = () => {
     };
     return (
         <nav className="navbar">
+            <img src={Logo} className="genLogo" alt="geneva logo" />
             <div className="container">
                 <div className="nav-elements">
                     <ul className="navList">
-                        <li>
-                            <img src={Logo} className="genLogo" alt="geneva logo" />
-                        </li>
                         <li>
                             <NavLink to="/">Home</NavLink>
                         </li>
