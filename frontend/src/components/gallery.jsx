66e1c25a3b8add55abd8560634fcbce0d10af568
MrHaydenn <haydenwheeler21@gmail.com>
2024-02-29 10:57:00 -0500

    profile / upload changes (not working)

--- frontend/src/components/gallery.jsx
+++ frontend/src/components/gallery.jsx
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../assets/table.css';
+import '../assets/gallery.css';
 
 const Table = () => {
     const [events, setEvents] = useState([]);
 
     useEffect(() => {
-        //http://127.0.0.1:8000/api/events/
-        //http://mrhaydenn.us.to:8000/api/events/
-        axios.get('http://mrhaydenn.us.to:8000/api/events/')
+        axios.get('http://mrhaydenn.us.to:8000/api/images/')
             .then(response => {
                 setEvents(response.data);
             })
             .catch(error => {
-                console.error('Error fetching events:', error);
+                console.error('Error fetching uploads:', error);
             });
     }, []);
 
@@ -22,21 +20,17 @@ const Table = () => {
             <table>
                 <thead>
                     <tr>
-                        <th>Title</th>
+                        <th>Selected Building</th>
                         <th>Description</th>
-                        <th>Event Date</th>
-                        <th>Event Location</th>
                         <th>Images</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {events.map(event => (
-                        <tr key={event.title}>
-                            <td>{event.title}</td>
-                            <td>{event.description}</td>
-                            <td>{event.date}</td>
-                            <td>{event.location}</td>
-                            <td>{event.image}</td>
+                    {events.map(upload => (
+                        <tr key={upload.description}>
+                            <td>{upload.selectedBuilding}</td>
+                            <td>{upload.description}</td>
+                            <td>{upload.image}</td>
                         </tr>
                     ))}
                 </tbody>
--- frontend/src/components/uploadForm.jsx
+++ frontend/src/components/uploadForm.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import "../assets/uploadForm.css";
 
-class UploadForm extends Component {
+/*   class UploadForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedBuilding: '',
             description: '',
-            file: null,
+            image: null,
         };
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
 
-        const { selectedBuilding, description, file } = this.state;
+        const { selectedBuilding, description, image } = this.state;
 
         // Handle form submission, e.g., send data to a server or perform an action
 
         console.log('Selected Building:', selectedBuilding);
         console.log('Description:', description);
-        console.log('Selected File:', file);
+        console.log('Selected Image:', image);
     };
 
     render() {
@@ -57,7 +58,7 @@ class UploadForm extends Component {
 
                     <center><label htmlFor="image"></label>
                         <input
-                            type="file"
+                            type="image"
                             id="image"
                             name="image"
                             accept="image/*"
@@ -76,5 +77,97 @@ class UploadForm extends Component {
 
 export default UploadForm;
 
+*/
 
+
+const UploadForm = () => {
+    const [formData, setFormData] = useState({
+      selectedBuilding: '',
+      description: '',
+      image: '',
+    });
+    const [submitted, setSubmitted] = useState(false);
+  
+    const handleInputChange = (upload) => {
+      setFormData({
+        ...formData,
+        [upload.target.name]: upload.target.value,
+      });
+    };
+  
+    const handleSubmit = async (upload) => {
+      upload.preventDefault();
+  
+      const uploadData = {
+        selectedBuilding: formData.selectedBuilding,
+        description: formData.description,
+        image: ImageData.image,
+      };
+  
+      try {
+        const response = await axios.post('http://mrhaydenn.us.to:8000/api/upload_images/', uploadData);
+        console.log('Image Created', response.data);
+        setSubmitted(true);
+  
+        setFormData({
+          selectedBuilding: '',
+          description: '',
+          image: '',
+        });
+      } catch (error) {
+        console.error('Image Creation failed', error);
+      }
+    };
+  
+  
+    return (
+      <div className='uploadForm'>
+        <h2>Upload Image</h2>
+        {submitted && <div className="success-message">Upload submitted successfully!</div>}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Building:</label>
+            
+            <label htmlFor="selectedBuilding">Select a Playlist:</label>
+                    <select
+                        id="selectedBuilding"
+                        name="selectedBuilding"
+                        value={this.state.selectedBuilding}
+                        onChange={this.handleInputChange}
+                        required
+                    >
+                        <option value="">Select a Building</option>
+                        <option value="oldMain">Old Main</option>
+                        <option value="s&e">S&E</option>
+                        <option value="northwood">Northwood</option>
+                    </select>
+            </div>
+          <div>
+            <label>Description:</label>
+            <input
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+              rows="4"
+              cols="50"
+            />
+          </div>
+          <div>
+            <label>image:</label>
+            <input
+              type="image"
+              name="image"
+              value={ImageData.image}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Submit Image</button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default UploadForm;
+  
 
