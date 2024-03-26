b33e2c6826954aaee3e2f1ba0d6a6f385ece4301
Carter Milroy <90055693+cartermilroy@users.noreply.github.com>
2024-03-26 12:11:56 -0400

    sprint3_v4

--- frontend/src/components/Buildings/Alex.jsx
+++ frontend/src/components/Buildings/Alex.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../navbar";
 import Footer from "../footer";
 import JumboAlex from "../jumboAlex";
+import '/Users/cmilr/OneDrive/Desktop/CSC483_2023_Team3/frontend/src/assets/Buildings/Alex.css';
 
 
 const Alex = () => {
@@ -12,13 +13,23 @@ const Alex = () => {
         <JumboAlex />
       </header>
       <main>
-        <div className="border">
-          <h5>Created By Zack Martin, Hayden Wheeler, & Carter Milroy</h5>
-          <div className="shape"></div>
+        <div id="pageTitle">
+          <h2>Alexander Hall</h2>
+        </div>
+        <div className="about">
+          <p>Alexander Hall, situated at the heart of Geneva College, stands as a symbol of the institution's rich history and academic legacy. Originally constructed in [year], this architectural masterpiece has been a witness to generations of students, faculty, and countless milestones in the college's journey.</p>
+          <p>The hall is named after [Name], one of the founding figures of Geneva College, whose vision and dedication played a pivotal role in shaping the institution. With its timeless design and central location on campus, Alexander Hall continues to serve as a hub for academic, cultural, and social activities.</p>
+        </div>
+        <div className="quickFacts">
+          <ul className="factsList">
+            <li>Founded in [Year]</li>
+            <li>Named after [Name]</li>
+            <li>Historical Signifigance</li>
+            <li>Notable Alumni</li>
+          </ul>          
         </div>
         <div className="gallery">
-          <h1 className="sectTitle">Gallery</h1>
-          <div className="col" id="imgPlace">
+          <div className="galleryImage">
               <img src="https://www.geneva.edu/campusmap/_assets/images/bld-alexander-1.jpg" alt="Alexander Hall"></img>
           </div>
         </div>
