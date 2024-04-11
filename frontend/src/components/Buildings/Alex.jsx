3996dc062fb8bcb2c2ee0fd292215d9c1ed08df4
Carter Milroy <90055693+cartermilroy@users.noreply.github.com>
2024-04-11 10:52:21 -0400

    bulding page tune up

--- frontend/src/components/Buildings/Alex.jsx
+++ frontend/src/components/Buildings/Alex.jsx
@@ -16,25 +16,27 @@ const Alex = () => {
         <div className="pageTitle">
           <h1>Alexander Hall</h1>
         </div>
-        <div className="displayLeft">
-          <div className="about">
-            <p>Alexander Hall is home to the main dining facility on campus, as well as the Admissions and Institutional Advancement Offices. Alexander Dining Hall was renovated in the summer of 2014, and includes a wide variety of food offerings.</p>
-            <p>Students, staff and faculty members often mingle and fellowship around tables in the dining hall, and large windows overlooking campus make it an inviting place to eat, converse or study.</p>
+        <div className="container">
+          <div className="displayLeft">
+            <div className="about">
+              <p>Alexander Hall is home to the main dining facility on campus, as well as the Admissions and Institutional Advancement Offices. Alexander Dining Hall was renovated in the summer of 2014, and includes a wide variety of food offerings.</p>
+              <p>Students, staff and faculty members often mingle and fellowship around tables in the dining hall, and large windows overlooking campus make it an inviting place to eat, converse or study.</p>
+            </div>          
           </div>
-          <div className="quickFacts">
-            <ul className="factsList">
-              <h2>Quick Facts</h2>
-              <li>Renovated in 2014</li>
-              <li>Serves dishes from areas top chef, Tom</li>
-              <li>Geneva College's primary Dining Hall</li>
-              <li>Features a self-serve Ice Cream bar</li>
-            </ul>          
-          </div>
-        </div>
-        <div className="displayRight">
-          <div className="gallery">
-            <div className="galleryImage">
-                <img src="https://www.geneva.edu/campusmap/_assets/images/bld-alexander-1.jpg" alt="Alexander Hall"></img>
+          <div className="displayRight">
+            <div className="gallery">
+              <div className="galleryImage">
+                  <img src="https://www.geneva.edu/campusmap/_assets/images/bld-alexander-1.jpg" alt="Alexander Hall"></img>
+              </div>
+            </div>
+            <div className="quickFacts">
+              <ul className="factsList">
+                <h2>Quick Facts</h2>
+                <li>Renovated in 2014</li>
+                <li>Serves dishes from areas top chef, Tom</li>
+                <li>Geneva College's primary Dining Hall</li>
+                <li>Features a self-serve Ice Cream bar</li>
+              </ul>
             </div>
           </div>
         </div>
